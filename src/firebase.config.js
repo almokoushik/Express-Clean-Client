fc70c9b2656c0e7e10cd721f98469087805f9547
MD AL MOTTAKI <mottaki567@gmail.com>
2021-04-30 14:36:57 +0600

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,11 +1,2 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyB-T3C4YYr7lJmsgRY3DR2ajU7QkZF4BFQ",
-    authDomain: "express-clean.firebaseapp.com",
-    projectId: "express-clean",
-    storageBucket: "express-clean.appspot.com",
-    messagingSenderId: "500581469015",
-    appId: "1:500581469015:web:5cb44ffcfb338313474c12",
-    measurementId: "G-EXKPZJTZJ5"
-};
-export default firebaseConfig;
+/////Firebase Config Needed For Project
